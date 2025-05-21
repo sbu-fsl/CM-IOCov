372d12d18632f5928399e279132b56afbc674d9f
Yifei Liu <yifeliu@cs.stonybrook.edu>
2025-05-21 19:31:42 -0400

    bug compare scripts

---
+++ fsl_scripts/bug_count_branches.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CM_IOCOV_PATH="/crashmonkey-iocov-part/cm-iocov-2025-0120/crashmonkey-iocov"
+ORIGINAL_CM_PATH="/crashmonkey-part/unmodified-crashmonkey-2025-0120/crashmonkey-iocov"
+
+CM_IOCOV_LOG="20250318_224432-xfsMonkey.log"
+ORIGINAL_CM_LOG="20250425_171944-xfsMonkey.log"
+
+
--- fsl_scripts/bugs_compare_branches.py
+++ fsl_scripts/bugs_compare_branches.py
@@ -2,12 +2,12 @@
 import os
 
 cm_iocov_path="/crashmonkey-iocov-part/cm-iocov-2025-0120/crashmonkey-iocov"
-original_cm_Path="/crashmonkey-part/unmodified-crashmonkey-2025-0120/crashmonkey-iocov"
+original_cm_path="/crashmonkey-part/unmodified-crashmonkey-2025-0120/crashmonkey-iocov"
 
 diff_dir = "diff_results"
 
 dir_cm_iocov = os.path.join(cm_iocov_path, diff_dir)
-dir_original_cm = os.path.join(original_cm_Path, diff_dir)
+dir_original_cm = os.path.join(original_cm_path, diff_dir)
 
 files_cm_iocov = set(f for f in os.listdir(dir_cm_iocov) if os.path.isfile(os.path.join(dir_cm_iocov, f)))
 files_original_cm = set(f for f in os.listdir(dir_original_cm) if os.path.isfile(os.path.join(dir_original_cm, f)))
@@ -15,9 +15,14 @@ files_original_cm = set(f for f in os.listdir(dir_original_cm) if os.path.isfile
 cm_iocov_only = list(files_cm_iocov - files_original_cm)
 original_cm_only = list(files_original_cm - files_cm_iocov)
 
-print("Only in cm_iocov:", cm_iocov_only)
-print("Only in original_cm:", original_cm_only)
-print("Length of cm_iocov bugs:", len(files_cm_iocov))
-print("Length of original_cm bugs:", len(files_original_cm))
-print("Length of cm_iocov only:", len(cm_iocov_only))
-print("Length of original_cm only:", len(original_cm_only))
+common_files = files_cm_iocov & files_original_cm
+
+print("Only in cm_iocov: ", cm_iocov_only)
+print("Only in original_cm: ", original_cm_only)
+print("Test failures on both branches: ", common_files)
+
+print("Length of cm_iocov bugs: ", len(files_cm_iocov))
+print("Length of original_cm bugs: ", len(files_original_cm))
+print("Length of cm_iocov only: ", len(cm_iocov_only))
+print("Length of original_cm only: ", len(original_cm_only))
+print("Length of test failures on both branches: ", len(common_files))
